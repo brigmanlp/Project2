c1c1f16c1a71a8d91908aeaef08296d311550c9e
Jasmine Bishop <jerezapo@uncc.edu>
2017-03-23 19:17:14 -0400

    finalized post.js

--- models/post.js
+++ models/post.js
@@ -1,21 +1,115 @@
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("Citizen Contribution", {
-    featureName: {
+    FeatureName: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Theme: {
+      type: DataTypes.STRING,
+      defaultValue: "Not Available"
+    },
+    FeatureLocation: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
     },
-    featureDetails: {
-      type: DataTypes.TEXT,
+    FeatureDetails: {
+      type: DataTypes.STRING,
       allowNull: false,
       len: [1]
     },
-    category: {
+     Latitude: {
+      type: DataTypes.NUMERIC,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Longitude: {
+      type: DataTypes.NUMERIC,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Image1Name: {
       type: DataTypes.STRING,
-      defaultValue: "Not Available"
+      validate: {
+        len: [1]
+      }
+    },
+    Image1Path: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Image1Description: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Image2Name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Image2Path: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Image2Description: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link1Name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link1Path: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link1Description: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link2Name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link2Path: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    Link2Description: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
     }
   });
+
   return Post;
 };
