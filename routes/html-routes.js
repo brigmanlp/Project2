23cee834c8a0f87224c0476e4d3c9723eeaf0dc0
Christina Ruc <Christina Ruc>
2017-03-30 20:58:54 -0400

    added wells and cleaned up imgs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../public/greenpoints.html"));
     });
 
-        app.get("/description", function(req, res) {
+    app.get("/description", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/description.html"));
     });
 };
