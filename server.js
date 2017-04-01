ae8151b65a2b910013f2322daec9e5d392ed7fbf
Louremy Pena <louremypena@wifi_guest-10-38-102-12.uncc.edu>
2017-04-01 10:23:40 -0400

    jaws db

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({ force: false }).then(function(data, error) {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
